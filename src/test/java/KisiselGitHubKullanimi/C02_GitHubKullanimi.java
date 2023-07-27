fef68bbe47c54a85862d2d31a1b23828904d0751
SuleymanEke <sknturk@gmail.com>
2023-07-27 21:38:04 +0300

    2. ders

---
+++ src/test/java/KisiselGitHubKullanimi/C02_GitHubKullanimi.java
@@ -0,0 +1,9 @@
+package KisiselGitHubKullanimi;
+
+public class C02_GitHubKullanimi {
+    public static void main(String[] args) {
+
+        System.out.println("Hello again, I am still using GitHub.");
+
+    }
+}
