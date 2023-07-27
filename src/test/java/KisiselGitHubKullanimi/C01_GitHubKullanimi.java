35ab87bcf0f1665787634d81e479d34c10cbbd8a
SuleymanEke <sknturk@gmail.com>
2023-07-27 21:25:07 +0300

    IlkDenemeGit

---
+++ src/test/java/KisiselGitHubKullanimi/C01_GitHubKullanimi.java
@@ -0,0 +1,12 @@
+package KisiselGitHubKullanimi;
+
+public class C01_GitHubKullanimi {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba, Ben GitHub Kullaniyorum.");
+        System.out.println("Hello, I am using GitHub.");
+        System.out.println("Здравствуйте, я использую GitHub.");
+    }
+
+}
+
