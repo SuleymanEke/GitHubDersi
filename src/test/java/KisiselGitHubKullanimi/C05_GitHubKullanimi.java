171e5b6c3a63c1566824389d00ec58152916314a
SuleymanEke <sknturk@gmail.com>
2023-07-28 19:11:01 +0300

    V4

---
+++ src/test/java/KisiselGitHubKullanimi/C05_GitHubKullanimi.java
@@ -0,0 +1,12 @@
+package KisiselGitHubKullanimi;
+
+public class C05_GitHubKullanimi {
+    public static void main(String[] args) {
+        System.out.println("Bu mesaji okuyorsan ben GitHub ogreniyorum demektir!");
+        System.out.println("If you are reading this message It means I am studying GitHub!");
+        System.out.println("あなたがこのメッセージを読んでいるなら、それは私がGitHubを勉強していることを意味します!");
+        System.out.println("Если вы читаете это сообщение, это означает, что я изучаю GitHub!");
+
+
+    }
+}
