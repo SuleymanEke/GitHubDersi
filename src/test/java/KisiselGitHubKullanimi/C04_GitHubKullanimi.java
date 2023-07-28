2ad9c053a3f5345174e6b8ad4c72025506a385fc
SuleymanEke <sknturk@gmail.com>
2023-07-28 19:01:43 +0300

    V2

---
+++ src/test/java/KisiselGitHubKullanimi/C04_GitHubKullanimi.java
@@ -0,0 +1,12 @@
+package KisiselGitHubKullanimi;
+
+public class C04_GitHubKullanimi {
+    public static void main(String[] args) {
+
+        System.out.println("GitHub ne kadar guzelmis !");
+        System.out.println("What a nice it is to use GitHub!");
+        System.out.println("GitHUBを使うのはなんて素敵なことでしょうか!");
+        System.out.println("Как приятно пользоваться GitHub!");
+
+    }
+}
---
+++ src/test/java/KisiselGitHubKullanimi/C0_GitHubKullanimi.java
@@ -0,0 +1,11 @@
+package KisiselGitHubKullanimi;
+
+public class C0_GitHubKullanimi {
+    public static void main(String[] args) {
+
+        System.out.println("Ben hala GitHub kullanmaya devam ediyorum.");
+        System.out.println("I am still using GitHub.");
+        System.out.println("私はまだGitHubを使用しています.");
+        System.out.println("Я все еще использую GitHub.");
+    }
+}
